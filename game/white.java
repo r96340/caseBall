bdb31b8ca2469a270b6aaa8c4ad705076481ebfc
r96340 <r96340@gmail.com>
2025-07-16 15:54:14 +0800

    Initialize white choice template

--- game/white.java
+++ game/white.java
@@ -9,10 +9,24 @@ public class white {
 
     //指定守位
     public static void setPositions() {
+
+        tools.clearScreen.all();
         System.out.println("========================================");
-        System.out.println("        請指定白隊捕手");
+        System.out.println("        電腦指定綠隊投手");
         System.out.println("========================================");
-        
+        for(int i=0;i<100;i++){
+            if(launch.registration[i] instanceof models.Pitcher){
+                launch.registration[i].showRegistry(i);
+            }
+        }
+        System.out.println("按輸入鍵繼續...");
+        launch.scanner.nextLine();
+        //初版強制選擇投手為 46號 黃群
+        int mockScanner = 46;
+        launch.fieldingGreen[1] = launch.registration[mockScanner];
+        System.out.println("電腦指定 " + mockScanner + "號 " + launch.fieldingGreen[1].name);
+        System.out.println("按輸入鍵繼續...");
+        launch.scanner.nextLine();
     }
 
 }
